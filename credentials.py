4a63adce70a34bcbdccca52700d152c2e9aed3c8
Dennis Mwaniki <damos5776@gmail.com>
2021-07-31 10:35:57 +0300

    import test framework unittest

---
+++ credentials.py
@@ -0,0 +1,15 @@
+
+class Credentials:
+    '''
+     credential class created
+    '''  
+
+    credential_list=[]
+
+
+def __init__(self,platform,password):
+    self.platform=platform
+    self.password=password
+
+
+   
---
+++ test_credentials.py
@@ -0,0 +1,5 @@
+#import test framework unittest
+#import credential class
+
+import unittest
+from credentials import Credentials
