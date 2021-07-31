1f02cc8df4b223f8dfa77ac9a6d5d862dc2c80d3
Dennis Mwaniki <damos5776@gmail.com>
2021-07-31 11:36:27 +0300

    save multiple credentials

--- credentials.py
+++ credentials.py
@@ -6,3 +6,11 @@ class Credentials:
     def __init__(self,platform,password):
         self.platform=platform
         self.password=password
+
+    def save_credentials(self):
+
+        '''
+          save_credentials method saves credentials objects into credentials_list
+        '''
+        
+        Credentials.credentials_list.append(self)
--- test_credentials.py
+++ test_credentials.py
@@ -9,6 +9,12 @@ from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
 
+    def tearDown(self):
+        '''
+        does a clean up after each task
+        '''
+        Credentials.credentials_list= []
+
     def setUp(self):
         '''
         Define a set of instructions to be executed before each task
@@ -20,8 +26,30 @@ class TestCredentials(unittest.TestCase):
         check if test object has be instatiated
         '''
         self.assertEqual(self.new_credentials.platform,'instagram')
-        self.assertEqual(self.new_credentials.password,'mwanikiiii')         
+        self.assertEqual(self.new_credentials.password,'mwanikiiii')       
 
+#save credentials test
+
+    def test_save_credentials(self):
+        '''
+        test to save contact
+        '''
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+#save multiple credentials
+
+    def test_save_multiple_credentials(self):
+        '''
+        test to check if multiple contacts can be saved
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials=Credentials('test','credentials')
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+    
+        
 
 if __name__=='__main__':
     unittest.main()
