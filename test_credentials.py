16ebe0d1bd054f822a8b805afa9b140324ce44b9
Dennis Mwaniki <damos5776@gmail.com>
2021-07-31 10:39:46 +0300

    set up method

--- test_credentials.py
+++ test_credentials.py
@@ -3,3 +3,14 @@
 
 import unittest
 from credentials import Credentials
+
+#create subclass
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Define a set of instructions to be executed before each task
+        '''
+        self.new_credentials=Credentials('instagram','mwanikiiii')
+        
